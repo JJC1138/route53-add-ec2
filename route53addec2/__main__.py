afe134dbcce8182fee3f4eaa7d60ffd4dde8b4bd
Jon Colverson <github@vcxz.co.uk>
2019-01-07 23:37:36 +0000

    Added argument parsing

--- route53addec2/__main__.py
+++ route53addec2/__main__.py
@@ -1,2 +1,10 @@
+import argparse
+import sys
+
 def main():
-    print("Hi")
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('instance_id', metavar='instance-id', help="The ID of the EC2 instance.")
+    arg_parser.add_argument('hostname', help="The hostname that you want to point to the EC2 instance. It must be in a Route 53 zone that you can modify.")
+    if len(sys.argv) == 1:
+        sys.argv.append('-h')
+    args = arg_parser.parse_args()
