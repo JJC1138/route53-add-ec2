731bcf616aff2654a1cf91c624bc3a2fd195aef9
Jon Colverson <github@vcxz.co.uk>
2019-01-08 01:09:47 +0000

    Added setting of the records on Route 53
    This code was copied almost verbatim from [route53dynamicdns](https://github.com/JJC1138/route53dynamicdns).

--- route53addec2/__main__.py
+++ route53addec2/__main__.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+import time
 
 import boto3
 
@@ -40,25 +41,6 @@ def main():
     else:
         log("Found zone ID %s" % zone_id)
     
-    log('Getting existing record\'s TTL')
-    
-    ttl = None
-    
-    record_sets = r53.list_resource_record_sets(
-        HostedZoneId=zone_id, StartRecordName=args.hostname, StartRecordType='A', MaxItems='1'
-        )['ResourceRecordSets']
-    if len(record_sets) > 0:
-        record_set = record_sets[0]
-        # Because we're only using StartRecordName to filter, if the record doesn't exist then we might have some other record:
-        if record_set['Name'] == args.hostname:
-            ttl = record_set['TTL']
-    
-    if ttl is None:
-        ttl = 60
-        log("No existing record found. New record will have TTL of %d" % ttl)
-    else:
-        log("Found existing record with TTL %d" % ttl)
-    
     ec2 = boto3.resource('ec2')
     
     log("Fetching EC2 instance information")
@@ -82,3 +64,88 @@ def main():
         log("No IPv6 address found")
     else:
         log("Found IPv6 address %s" % ipv6_address)
+    
+    changes = []
+    
+    for doing_ipv6_change in (False, True):
+        record_type = 'AAAA' if doing_ipv6_change else 'A'
+        ip = ipv6_address if doing_ipv6_change else ipv4_address
+        ip_version_string = 'IPv6' if doing_ipv6_change else 'IPv4'
+        
+        record_sets = r53.list_resource_record_sets(
+            HostedZoneId=zone_id, StartRecordName=args.hostname, StartRecordType=record_type, MaxItems='1'
+        )['ResourceRecordSets']
+        
+        existing_ttl = None
+        
+        if len(record_sets) > 1:
+            # This shouldn't be able to happen as far as I'm aware.
+            raise Exception("Multiple record sets returned, and we don't know how to handle that.")
+        
+        elif len(record_sets) == 1:
+            record_set = record_sets[0]
+            
+            if record_set['Type'] == record_type and (record_set['Name'] == args.hostname or record_set['Name'] == '%s.' % args.hostname):
+                records = record_set.get('ResourceRecords', [])
+            else:
+                # The API has returned some other record set which means there wasn't an exact match for the one we asked for.
+                records = []
+            
+            if len(records) > 1:
+                raise Exception("Multiple addresses found on %s record so this record has been modified by someone or something other than this tool. Please delete all but one of the values to use the record with this tool." % record_type)
+            
+            elif len(records) == 1:
+                existing_ttl = record_set['TTL']
+                existing_ip = records[0]['Value']
+                
+                if existing_ip == ip:
+                    log("Record is already set to correct %s address %s" % (ip_version_string, ip))
+                    continue
+                else:
+                    log("Found existing %s record with TTL of %d" % (ip_version_string, existing_ttl))
+            
+            elif len(records) == 0:
+                record_sets = []
+        
+        if len(record_sets) == 0:
+            if ip is None:
+                log("No %s address record exists and we don't have an address of that type" % ip_version_string)
+                continue
+        
+        if ip:
+            ttl = existing_ttl or 60
+            log("Setting record to point to %s address %s with TTL %d" % (ip_version_string, ip, ttl))
+        else:
+            ttl = existing_ttl
+            log("Removing record for %s address because we don't have an address of that type" % ip_version_string)
+        
+        change = {
+            'Action': 'UPSERT' if ip is not None else 'DELETE',
+            'ResourceRecordSet': {
+                'Name': args.hostname,
+                'Type': record_type,
+                'TTL': ttl,
+                'ResourceRecords': [
+                    {
+                        'Value': ip or existing_ip,
+                    }
+                ],
+            },
+        }
+        
+        changes.append(change)
+    
+    if len(changes) == 0:
+        # All records are correct already.
+        return
+    
+    response = r53.change_resource_record_sets(
+        HostedZoneId = zone_id,
+        ChangeBatch = { 'Changes': changes })
+    
+    while response['ChangeInfo']['Status'] != 'INSYNC':
+        log("Waiting for DNS update to propagate")
+        time.sleep(15)
+        response = r53.get_change(Id=response['ChangeInfo']['Id'])
+    
+    log("DNS has finished propagating")
